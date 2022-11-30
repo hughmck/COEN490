0b62112193d80026537da09e01b204a2aa6cb6e9
hugh mckenzie <64859308+hughmck@users.noreply.github.com>
2022-11-29 20:13:08 -0500

    nothing should be changed

--- coen490/src/pages/HCP/HCP-profile.js
+++ coen490/src/pages/HCP/HCP-profile.js
@@ -1,11 +1,14 @@
+import React from 'react';
 import { Container } from "react-bootstrap"
 import { AuthProvider } from "../../contexts/AuthContext"
 import LogOut from '../Authentification/logout'
+
 
 export default function HCPProfile(){
 
 
   return (
+
     <AuthProvider>
       <Container className = "d-flex align-items-center justify-content-center" style = {{ minHeight: "100vh" }}>
         <div className = "w-100" style ={{ maxWidth: "400px"}}>
