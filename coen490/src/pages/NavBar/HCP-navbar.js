75e2bf9ed31baa305c13756f75686ab8f244e4c5
hugh mckenzie <64859308+hughmck@users.noreply.github.com>
2023-03-09 17:01:05 -0500

    adding more front end

--- coen490/src/pages/NavBar/HCP-navbar.js
+++ coen490/src/pages/NavBar/HCP-navbar.js
@@ -1,30 +1,35 @@
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 
-
-export default function HomeNavBar(){
-
-  return(
-    <nav className = "nav">
-      <Link to = "/" className = "site_title">
-        EasySante
-      </Link>
-
-      <ul>
-      <CustomLink to="/HCP-dashboard"> DashBoard </CustomLink>
-      <CustomLink to="/HCP-connect"> Connect </CustomLink>
-      <CustomLink to="/HCP-patient-list"> Patient Info </CustomLink>
-      <CustomLink to="/HCP-profile"> View Profile </CustomLink>
-      </ul>
+export default function HomeNavBar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div className="container-fluid">
+        <Link to="/" className="navbar-brand">
+          <img src={process.env.PUBLIC_URL + '/logo-noback.png'} alt="logo" className="d-inline-block align-text-top me-2" style={{height: "2rem"}} />
+          EasySante
+        </Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+          <ul className="navbar-nav">
+            <CustomLink to="/HCP-dashboard" className="nav-link"> DashBoard </CustomLink>
+            <CustomLink to="/HCP-connect" className="nav-link"> Connect </CustomLink>
+            <CustomLink to="/HCP-patient-list" className="nav-link"> Patient Info </CustomLink>
+            <CustomLink to="/HCP-profile" className="nav-link"> View Profile </CustomLink>
+            <CustomLink to="/logout" className="nav-link"> Log Out </CustomLink>
+          </ul>
+        </div>
+      </div>
     </nav>
   )
-
 }
 
 function CustomLink({to, children, ...props}) {
   const path = window.location.pathname
-  return(
-  <li className = {path === to ? "active" : ""}>
-    <Link to={to} {...props}>{children}</Link>
-  </li>
+  return (
+    <li className={path === to ? "nav-item active" : "nav-item"}>
+      <Link to={to} {...props}>{children}</Link>
+    </li>
   )
 }
