b6daac3d754247f0673ee0eacf5ae0c84271a5bf
hugh mckenzie <64859308+hughmck@users.noreply.github.com>
2023-03-11 16:01:35 -0500

    updating homepage

--- coen490/src/pages/home.js
+++ coen490/src/pages/home.js
@@ -2,12 +2,14 @@ import { useContext } from "react";
 import { UserContext } from "../contexts/user.context";
 
 export default function Home() {
-
- const { user } = useContext(UserContext);
-  if (!user) {
-    console.log('Not Logged In');
-  }
+  const { user } = useContext(UserContext);
   return(
- <h1>Home Page (No User Logged In)</h1>
-)
+    <div>
+      {!user && (
+        <div style={{ backgroundColor: "#f8d7da", color: "#721c24", padding: "0.75rem 1.25rem", marginBottom: "1rem", border: "1px solid #f5c6cb" }}>
+          You are not currently signed-in. Please log in.
+        </div>
+      )}
+    </div>
+  )
 }
