0fa109f0dd7580de0c4535a4e0d8b34e0c33e1c6
antoinegaubil <antoinegaubil@gmail.com>
2023-03-12 16:39:11 -0400

    not sure

--- coen490/src/pages/USER/user-dashboard.js
+++ coen490/src/pages/USER/user-dashboard.js
@@ -1,21 +1,43 @@
-import { UserContext } from "../../contexts/user.context";
-import { App, Credentials } from "realm-web";
-import axios from 'axios';
-import { useContext, useState, useEffect } from "react";
-import '../../style/user/user-dashboard.css';
+import React, { useState, useEffect } from 'react';
+import { Tweet } from 'react-twitter-widgets';
 
 export default function UserDashboard() {
-  const [showPopup, setShowPopup] = useState(false);
+  const [tweetId, setTweetId] = useState('');
 
   useEffect(() => {
+    const getLatestTweetId = async () => {
+      const latestTweetId = await fetchLatestTweetId();
+      setTweetId(latestTweetId);
+    }
 
+    const fetchLatestTweetId = async () => {
+      /*const response = await fetch('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=<screen_name>&count=1', {
+        headers: {
+          'Authorization': 'Bearer <bearer_token>'
+        }
+      });
+      const data = await response.json();
+      */
+      return "1626232503534256131";
+    };
+
+    const getMsUntilNextHour = () => {
+      const now = new Date();
+      const nextHour = new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours() + 1, 0, 0);
+      console.log(nextHour.getTime() - now.getTime())
+      return nextHour.getTime() - now.getTime();
+    }
+
+    const interval = setInterval(() => {
+      getLatestTweetId();
+    }, getMsUntilNextHour());
+
+    return () => clearInterval(interval);
   }, []);
 
-
-
-  
   return (
     <>
+      {tweetId && <Tweet tweetId={tweetId} />}
       <h1>USER</h1>
     </>
   );
