38cc6c90bb84064bd3c08488313af3c69285499b
hugh mckenzie <64859308+hughmck@users.noreply.github.com>
2023-03-13 17:13:01 -0400

    working on welcome page

--- coen490/src/pages/home.js
+++ coen490/src/pages/home.js
@@ -2,38 +2,45 @@ import { useContext } from "react";
 import { UserContext } from "../contexts/user.context";
 import { Typography, Paper, Grid, Box, Accordion, AccordionSummary, AccordionDetails } from "@mui/material";
 
+import customer1Image from "./download-1.jpg";
+import customer2Image from "./download-2.jpg";
+import customer3Image from "./download-3.jpg";
+import customer4Image from "./minionbrah.jpg";
+import customer5Image from "./Sad-Minion.jpg";
+
+const reviews = [
+  {
+    name: "Jane",
+    quote: "I am so grateful to have found this community. The support and understanding I have received here has helped me to feel less alone and more empowered in my mental health journey.",
+    image: customer1Image,
+  },
+  {
+    name: "John",
+    quote: "The resources provided by this community have been invaluable in helping me to better understand and manage my mental health. Thank you!",
+    image: customer2Image,
+  },
+  {
+    name: "Sarah",
+    quote: "I appreciate the non-judgmental and supportive environment that this community provides. It has been a lifeline for me during a difficult time.",
+    image: customer3Image,
+  },
+  {
+    name: "Michael",
+    quote: "EasySante has been a game-changer for my mental health. The app has helped me to track my sleep and stress levels and provided me with tailored recommendations for how to improve them.",
+    image: customer4Image,
+  },
+  {
+    name: "Emily",
+    quote: "The therapists I have worked with through EasySante have been incredibly knowledgeable and supportive. I have felt heard and understood throughout the entire process.",
+    image: customer5Image,
+  }
+];
+
 export default function Home() {
   const { user } = useContext(UserContext);
-  const reviews = [
-    {
-      name: "Jane",
-      quote: "I am so grateful to have found this community. The support and understanding I have received here has helped me to feel less alone and more empowered in my mental health journey."
-    },
-    {
-      name: "John",
-      quote: "The resources provided by this community have been invaluable in helping me to better understand and manage my mental health. Thank you!"
-    },
-    {
-      name: "Sarah",
-      quote: "I appreciate the non-judgmental and supportive environment that this community provides. It has been a lifeline for me during a difficult time."
-    },
-    {
-      name: "Michael",
-      quote: "EasySante has been a game-changer for my mental health. The app has helped me to track my sleep and stress levels and provided me with tailored recommendations for how to improve them."
-    },
-    {
-      name: "Emily",
-      quote: "The therapists I have worked with through EasySante have been incredibly knowledgeable and supportive. I have felt heard and understood throughout the entire process."
-    }
-  ];
   
   return (
     <Box sx={{ p: 2 }}>
-      {/* {!user && (
-        <Paper elevation={3} sx={{ backgroundColor: "#f8d7da", color: "#721c24", padding: "0.75rem 1.25rem", marginBottom: "1rem", border: "1px solid #f5c6cb" }}>
-          You are not currently signed-in. Please log in.
-        </Paper>
-      )} */}
       <Typography variant="h1" align="center" gutterBottom>
         Welcome to EasySante!
       </Typography>
@@ -46,11 +53,21 @@ export default function Home() {
       <Grid container spacing={3} justifyContent="center">
         {reviews.map((review, index) => (
           <Grid item xs={12} sm={6} md={4} key={index}>
-            <Accordion>
-              <AccordionDetails>
-                <Typography variant="body1" align="center">{review.quote}</Typography>
-              </AccordionDetails>
-            </Accordion>
+            <Paper sx={{ p: 2 }}>
+              <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                <Box sx={{ width: '100%', height: '200px', marginBottom: '16px' }}>
+                  <img src={review.image} alt={review.name} style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+                </Box>
+                <Accordion>
+                  <AccordionSummary>
+                    <Typography variant="h5">{review.name}</Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <Typography variant="body1" align="center">{review.quote}</Typography>
+                  </AccordionDetails>
+                </Accordion>
+              </Box>
+            </Paper>
           </Grid>
         ))}
       </Grid>
