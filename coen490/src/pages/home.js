b8b0cd77a67836b90c96da68c93685e58b9bafb7
hugh mckenzie <64859308+hughmck@users.noreply.github.com>
2023-03-17 16:39:40 -0400

    changing home page

--- coen490/src/pages/home.js
+++ coen490/src/pages/home.js
@@ -1,114 +1,31 @@
 import { useContext } from "react";
 import { UserContext } from "../contexts/user.context";
-import { Typography, Paper, Grid, Box, Accordion, AccordionSummary, AccordionDetails } from "@mui/material";
-import {
-  MDBCarousel,
-  MDBCarouselItem,
-  MDBBtn,
-  MDBFooter, MDBContainer, MDBRow, MDBCol, MDBInput
-} from 'mdb-react-ui-kit';
-
-import customer1Image from "./download-7.jpg";
-import customer2Image from "./download-8.jpg";
-import customer3Image from "./download-9.jpg";
-import customer4Image from "./download-10.jpg";
-import customer5Image from "./download-12.avif";
-
-const reviews = [
-  {
-    name: "Jane",
-    quote: "I am so grateful to have found this community. The support and understanding I have received here has helped me to feel less alone and more empowered in my mental health journey.",
-    image: customer1Image,
-  },
-  {
-    name: "John",
-    quote: "The resources provided by this community have been invaluable in helping me to better understand and manage my mental health. Thank you!",
-    image: customer2Image,
-  },
-  {
-    name: "Sarah",
-    quote: "I appreciate the non-judgmental and supportive environment that this community provides. It has been a lifeline for me during a difficult time.",
-    image: customer3Image,
-  },
-  {
-    name: "Michael",
-    quote: "EasySante has been a game-changer for my mental health. The app has helped me to track my sleep and stress levels and provided me with tailored recommendations for how to improve them.",
-    image: customer4Image,
-  },
-  {
-    name: "Emily",
-    quote: "The therapists I have worked with through EasySante have been incredibly knowledgeable and supportive. I have felt heard and understood throughout the entire process.",
-    image: customer5Image,
-  }
-];
+import '../style/home.css';
 
 export default function Home() {
   const { user } = useContext(UserContext);
   
   return (
-    <>
-      <MDBContainer style={{ marginBottom: '500px' }}>
-    <Box sx={{ p: 2, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '60vh' }}>
-  <Typography variant="h1" align="center" gutterBottom sx={{ fontSize: '3rem', fontWeight: 'bold', color: '#0D47A1', my: 0 }}>
-    Join over 1500 registered users who trust EasySante
-  </Typography>
-  <Typography variant="body1" align="center" gutterBottom sx={{ fontSize: '1rem', color: '#333', my: 4 }}>
-    We at EasySante believe that support, exercise, and a good night's sleep are the three pillars to improving your mental health. Luckily for you, we provide all three. EasySante provides simple and effective access to healthcare professionals which are tailored to your needs, while also providing you with useful insights on your sleep quality and stress levels.
-  </Typography>
-  <a href="/sign-up">
-    <MDBBtn className='me-1' align="center" color='success'>
-      Get Started!
-    </MDBBtn>
-  </a>
-  <Typography variant="h2" align="center" gutterBottom sx={{ fontSize: '2.5rem', fontWeight: 'bold', color: '#0D47A1', my: 4 }}>
-    What our customers are saying
-  </Typography>
-  <MDBCarousel showControls showIndicators style={{ width: '75%', height: '200px', margin: '0 auto', zIndex: 1 }}>
-    {reviews.map((review, index) => (
-      <MDBCarouselItem
-        key={index}
-        className='w-100 dock'
-        src={review.image}
-        alt={review.name}
-      >
-        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '20px', mb: 4 }}>
-          <Typography variant="h5" sx={{ color: 'white', fontSize: '2rem' }} fontWeight="bold">{review.name}</Typography>
-          <Typography variant="body1" align="center" sx={{ color: 'white', fontSize: '1rem' }}>{review.quote}</Typography>
-        </Box>
-      </MDBCarouselItem>
-    ))}
-  </MDBCarousel>
-</Box>
-</MDBContainer>
-
-<MDBFooter style={{ backgroundColor: 'blue' }} className='text-center text-lg-left'>
-  <MDBContainer className='p-4 pb-0'>
-    <form action=''>
-      <MDBRow className='d-flex justify-content-center'>
-        <MDBCol size='auto' className='mb-4 mb-md-0'>
-          <p className='pt-2' style={{ color: 'white' }}>
-            <strong>Sign up for our newsletter</strong>
-          </p>
-        </MDBCol>
-
-        <MDBCol md='5' size='12' className='mb-4 mb-md-0'>
-          <MDBInput type='text' id='form5Example2' style={{ color: 'white' }} label='Email address' />
-        </MDBCol>
-
-        <MDBCol size='auto' className='mb-4 mb-md-0'>
-          <MDBBtn style={{ color: 'white' }}>Subscribe</MDBBtn>
-        </MDBCol>
-      </MDBRow>
-    </form>
-  </MDBContainer>
-
-  <div className='text-center p-3' style={{ color: 'white' }}>
-    &copy; {new Date().getFullYear()} Copyright:{' '}
-    <a className='text-dark' href='https://mdbootstrap.com/'style={{ color: 'white' }}>
-      EasySante.com
-    </a>
-  </div>
-</MDBFooter>
-    </>
+    <main className="hero-section">
+  <section className="container">
+    <div className="hero-content">
+      <nav className="navbar">
+        <h1 className="nav-logo">EasySante</h1>
+        <ul className="nav-links">
+          <a href="/sign-up">Sign Up</a>
+          <a href="/sign-in">Sign In</a>
+        </ul>
+      </nav>
+      <div className="hero-text">
+        <h2 className="hero-welcome-text">Welcome To</h2>
+        <h1 className="hero-country">EasySante</h1>
+        <p className="hero-text-description">We at EasySante believe that support, exercise, and a good night's sleep are the three pillars to improving your mental health. Luckily for you, we provide all three. EasySante provides simple and effective access to healthcare professionals which are tailored to your needs, while also providing you with useful insights on your sleep quality and stress levels.</p>
+        <a href="/sign-up">
+          <button className="explore-btn">Get Started!</button>
+          </a>
+      </div>
+    </div>
+  </section>
+</main>
   )
 }
